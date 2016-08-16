79373cc9a7f862e2ea890eade5b8abf7ff6b7cd4
Kelly Lunghamer <kellylunghamer@kellys-mbp.attlocal.net>
2016-08-16 16:25:33 -0400

    Unit 10 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,20 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
+/*if (cardTwo === cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
+} */
+
+var x = document.getElementById('game-board');
+
+var createBoard = function () {
+	for(var i = 0; i < 4; i++) {
+	var newElement = document.createElement('div');
+	newElement.className = 'board';
+	x.appendChild(newElement);
+  }
 }
+
+createBoard();
